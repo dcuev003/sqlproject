bd815c6903b4eb9802db3960e0bf43f9739a1dbf
dcuev003 <49379251+dcuev003@users.noreply.github.com>
2020-06-09 18:15:59 -0700

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-root=$(realpath $(dirname "$0"))
-root=$(dirname $root)
-dbname=$(logname)_db
-
-cd $root/java
-
-# Example: ./run.sh
-java -cp lib/*:bin/ Ticketmaster $dbname $PGPORT $(logname)
