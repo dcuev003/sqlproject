ad3bb9f5ba6344e7f0b2e461b90f82e85ac33a3e
dcuev003 <49379251+dcuev003@users.noreply.github.com>
2020-06-09 18:16:13 -0700

    Delete stopdb.sh

--- stopdb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-folder=/tmp/$(logname)/mydb
-PGDATA=$folder/data
-PGSOCKETS=$folder/sockets
-export PGDATA
-export PGSOCKETS
-pg_ctl -o "-c unix_socket_directories=$PGSOCKETS" -D $PGDATA -l $folder/logfile stop
